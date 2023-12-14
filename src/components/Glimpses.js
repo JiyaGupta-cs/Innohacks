8f51aeaf3b32d4b1611c6810f7a234dd8873672a
Jiya <coderaholic21@gmail.com>
2023-12-14 19:31:31 +0530

    Updated glipmses images

--- src/components/Glimpses.js
+++ src/components/Glimpses.js
@@ -6,11 +6,11 @@ const Glimpses = () => {
     <div className='glimpses'>
       <div className="left">
       <div class="gallery">
-  <img src="https://picsum.photos/id/1004/400/400" alt="a lovely kiss in the night"/>
-  <img src="https://picsum.photos/id/1013/400/400" alt="a women inside a car"/>
-  <img src="https://picsum.photos/id/1066/400/400" alt="a baby"/>
-  <img src="https://picsum.photos/id/325/400/400" alt="a girl in the forest"/>
-  <img src="https://picsum.photos/id/65/400/400" alt="a girl"/>
+  <img src="https://www.kiet.edu/uploads/media/club_gallery_image/947439.jpg" alt="a lovely kiss in the night"/>
+  <img src="https://www.kiet.edu/uploads/media/club_gallery_image/1137223.jpg" alt="a women inside a car"/>
+  <img src="https://www.kiet.edu/uploads/media/club_gallery_image/5546764.jpg" alt="a baby"/>
+  <img src="https://www.kiet.edu/uploads/media/club_gallery_image/7522189.jpg" alt="a girl in the forest"/>
+  <img src="https://www.kiet.edu/uploads/media/club_gallery_image/4194458.jpg" alt="a girl"/>
 </div>
       </div>
       <div className="right">
